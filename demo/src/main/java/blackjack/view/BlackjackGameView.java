bd35d55f20390e121399a1527a04755f91e00057
Serife Aynur Kocdas <kocdasserifeaynur@gmail.com>
2026-05-07 11:14:38 -0700

    Revert "Connect Blackjack view to game logic"
    This reverts commit b7288c9c4781e027f82cbf59af3f3d64e763fe4e.

--- demo/src/main/java/blackjack/view/BlackjackGameView.java
+++ demo/src/main/java/blackjack/view/BlackjackGameView.java
@@ -1,41 +1,22 @@
 package blackjack.view;
 
-import blackjack.controller.BlackjackGameLogic;
-import blackjack.model.BlackjackPlayer;
-import blackjack.model.Dealer;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
-import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.Region;
 import javafx.scene.layout.VBox;
+
 import manager.controller.NavigationController;
 
+//Blackjack game interface
 public class BlackjackGameView {
 
     private final NavigationController nav;
     private final VBox root;
-    private final BlackjackGameLogic logic;
-
-    private final Label playerCardsLabel;
-    private final Label dealerCardsLabel;
-    private final Label balanceLabel;
-    private final Label statusLabel;
 
     public BlackjackGameView(NavigationController nav) {
         this.nav = nav;
-        this.logic = new BlackjackGameLogic();
-
-        logic.startNewRound(100);
-
-        playerCardsLabel = new Label();
-        dealerCardsLabel = new Label();
-        balanceLabel = new Label();
-        statusLabel = new Label("Game Started");
-
         this.root = build();
-
-        updateView();
     }
 
     public Region getRoot() {
@@ -43,61 +24,12 @@ public class BlackjackGameView {
     }
 
     private VBox build() {
-
         Label title = new Label("Blackjack Game");
         title.setStyle("-fx-font-size: 28px; -fx-font-weight: bold;");
 
-        Button hitButton = new Button("Hit");
-        Button standButton = new Button("Stand");
-
-        hitButton.setOnAction(e -> {
-            logic.humanHit();
-
-            if (logic.isHumanBust()) {
-                statusLabel.setText("Player Bust!");
-            }
-
-            updateView();
-        });
-
-        standButton.setOnAction(e -> {
-            logic.humanStand();
-            statusLabel.setText("Round Finished");
-            updateView();
-        });
-
-        VBox box = new VBox(
-                15,
-                title,
-                dealerCardsLabel,
-                playerCardsLabel,
-                balanceLabel,
-                statusLabel,
-                hitButton,
-                standButton
-        );
-
+        VBox box = new VBox(12, title);
         box.setAlignment(Pos.CENTER);
         box.setPadding(new Insets(40));
-
         return box;
-    }
-
-    private void updateView() {
-
-        BlackjackPlayer player = logic.getHumanPlayer();
-        Dealer dealer = logic.getDealer();
-
-        playerCardsLabel.setText(
-                "Player Hand: " + player.getHand()
-        );
-
-        dealerCardsLabel.setText(
-                "Dealer Hand: " + dealer.getHand()
-        );
-
-        balanceLabel.setText(
-                "Balance: $" + player.getBalance()
-        );
     }
 }
