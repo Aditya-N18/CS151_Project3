5dc830d844a513b2f36ca41315e3554994199e60
adithiv3 <adithivuligonda8@gmail.com>
2026-05-07 18:32:54 -0700

    Implemented Snake.java, Food.java, and Direction.java

---
+++ demo/src/main/java/snake/controller/SnakeGameLogic.java
@@ -0,0 +1,8 @@
+package snake.controller;
+
+import snake.model.*;
+public class SnakeGameLogic {
+    public SnakeGameLogic(int rows, int cols){
+
+    }
+}
---
+++ demo/src/main/java/snake/model/Food.java
@@ -0,0 +1,17 @@
+package snake.model;
+
+public class Food {
+    private Position position;
+
+    public Food(Position position){
+        this.position = position;
+    }
+
+    public Position getPosition(){
+        return position;
+    }
+
+    public void setPosition(Position position){
+        this.position = position;
+    }
+}
